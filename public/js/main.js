40700a4590731af5311646bab0d6714f3b3a6d2e
François Mentec <francois.mentec@alten.com>
2023-05-29 15:11:13 +0200

    new style + date

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,36 @@
-import { get_cookie } from './util.js'
+import { DAYS, MONTHS, get_cookie, th } from './util.js'
+
+const VIEWSTATE = parseInt(get_cookie('viewstate')) || 640
+const RING_BORDER = 40
+const RING_RADIUS = (VIEWSTATE - RING_BORDER) / 2
+
+const CPU_MAX_TEMP = 100
+const GPU_MAX_TEMP = 100
 
 const svg = document.getElementById('svg')
+const cpu_bar = document.getElementById('cpu_bar')
+const gpu_bar = document.getElementById('gpu_bar')
+const date_div = document.getElementById('date')
 
 // Set dimensions
-const VIEWSTATE = parseInt(get_cookie('viewstate')) || 640
 document.documentElement.style.width = VIEWSTATE + 'px'
 document.documentElement.style.height = VIEWSTATE + 'px'
 document.body.style.width = VIEWSTATE + 'px'
 document.body.style.height = VIEWSTATE + 'px'
 svg.setAttribute('width', VIEWSTATE)
 svg.setAttribute('height', VIEWSTATE)
+
+// Set up text paths
+{
+    const TEXT_HEIGHT = 50 
+    const center = VIEWSTATE / 2
+    const p1 = RING_BORDER / 2 + TEXT_HEIGHT
+    const p2 = VIEWSTATE - RING_BORDER / 2 - TEXT_HEIGHT
+    const RADIUS = RING_RADIUS - TEXT_HEIGHT
+    document.getElementById('cpu_label_path').setAttribute('d', `M ${center} ${p2} A ${RADIUS} ${RADIUS} 0 0 1 ${center} ${p1}`)
+    document.getElementById('gpu_label_path').setAttribute('d', `M ${center} ${p1} A ${RADIUS} ${RADIUS} 0 0 1 ${center} ${p2}`)
+    document.getElementById('date_path').setAttribute('d', `M ${p1} ${center} A ${RADIUS} ${RADIUS} 0 0 0 ${p2} ${center}`)
+}
 
 const cpu_temp = document.getElementById('cpu_temp')
 const gpu_temp = document.getElementById('gpu_temp')
@@ -23,40 +44,35 @@ window.nzxt = {
     }
 }
 
-const OUTER_RING_WIDTH = 40
-const RING_RADIUS = (VIEWSTATE - OUTER_RING_WIDTH) / 2
-
-
-const CPU_MAX_TEMP = 100
-const cpu_bar = document.getElementById('cpu_bar')
-
 function update_cpu (temp) {
-    cpu_temp.innerText = Math.round(temp)
+    cpu_temp.innerText = Math.round(temp) + '°'
 
     const perpi = temp / CPU_MAX_TEMP * Math.PI / 2
-    const x = OUTER_RING_WIDTH / 2 + RING_RADIUS * (1 - Math.cos(perpi))
-
+    const x = RING_BORDER / 2 + RING_RADIUS * (1 - Math.cos(perpi))
     const y1 = VIEWSTATE / 2 + RING_RADIUS * Math.sin(perpi)
     const y2 = VIEWSTATE / 2 - RING_RADIUS * Math.sin(perpi)
-
     cpu_bar.setAttribute('d', `M ${x} ${y1} A ${RING_RADIUS} ${RING_RADIUS} 0 0 1 ${x} ${y2}`)
 }
 
-
-const GPU_MAX_TEMP = 100
-const gpu_bar = document.getElementById('gpu_bar')
-
 function update_gpu (temp) {
-    gpu_temp.innerText = Math.round(temp)
+    gpu_temp.innerText = Math.round(temp) + '°'
 
     const perpi = temp / GPU_MAX_TEMP * Math.PI / 2
     const x = VIEWSTATE / 2 + RING_RADIUS * Math.cos(perpi)
-
     const y1 = VIEWSTATE / 2 + RING_RADIUS * Math.sin(perpi)
     const y2 = VIEWSTATE / 2 - RING_RADIUS * Math.sin(perpi)
-
     gpu_bar.setAttribute('d', `M ${x} ${y1} A ${RING_RADIUS} ${RING_RADIUS} 0 0 0 ${x} ${y2}`)
 }
 
-update_cpu(30)
-update_gpu(50)
+/*update_cpu(30)
+update_gpu(50)*/
+
+function update_date () {
+    const now = new Date()
+    date_div.innerHTML = `${now.getDate()} ${MONTHS[now.getMonth()]} ${now.getFullYear()}`
+    const tomorrow = new Date()
+    tomorrow.setHours(24, 0, 0, 0)
+    setTimeout(update_date, tomorrow - now)
+}
+
+update_date()
--- public/js/util.js
+++ public/js/util.js
@@ -1,3 +1,6 @@
+export const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 export function get_cookie (cookie_name) {
     const cookies = document.cookie.split(';')
     for (let cookie of cookies) {
@@ -9,4 +12,13 @@ export function get_cookie (cookie_name) {
         }
     }
     return null
+}
+
+export function th (number) {
+    let res = number
+    if (number % 10 === 1) res += 'st'
+    else if (number % 10 === 2) res += 'nd'
+    else if (number % 10 === 3) res += 'rd'
+    else res += 'th'
+    return res
 }
